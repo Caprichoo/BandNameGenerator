1a1e07dc8d23a5f95f36ffda4767ebd57a240fdf
Caprichoo <sandeepkb3899@outlook.com>
2024-02-24 20:30:29 +0530

    cd BandNameGenerator

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 print("Welcome to Band Name Generator!")
 city = input("What's your City Name: ")
 Pet_Name = input("What's your Pet_Name: ")
-print("Your Band Name could be: " + city + " " + Pet_Name);
-
-
-
+print("Your Band Name could be: " + city + " " + Pet_Name)
